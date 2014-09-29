83f612e07fa194f68fc100679cbe2fedb0e9e40c
nicolezhu <nzhu94@gmail.com>
2014-09-28 21:43:38 -0500

    style homepage and buttons

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,4 +1,15 @@
-var monkeyList = new List('test-list', { 
-  valueNames: ['name'], 
-  plugins: [ ListFuzzySearch() ] 
+$(document).ready(function() {
+	var monkeyList = new List('test-list', { 
+	  valueNames: ['name'], 
+	  plugins: [ ListFuzzySearch() ] 
+	});
+
+	$('.add').click(function(){
+	  $(this).toggleClass('selected');
+	  if ($(this).hasClass('selected')) {
+	  	$(this).html('&#x2713;');
+	  } else {
+	  	$(this).html('Add');
+	  }
+	});
 });
