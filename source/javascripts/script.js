ab1198eb5c8e745bc27dc50a6e97b6c84ae26db3
nicolezhu <nzhu94@gmail.com>
2014-09-29 00:38:26 -0500

    add friends icons

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -6,10 +6,27 @@ $(document).ready(function() {
 
 	$('.add').click(function(){
 	  $(this).toggleClass('selected');
+	  $(this).parent('li').toggleClass('added');
 	  if ($(this).hasClass('selected')) {
 	  	$(this).html('&#x2713;');
 	  } else {
 	  	$(this).html('Add');
 	  }
+	});
+
+	$('#classes').click(function() {
+		console.log('classes');
+		$('.list').children().each(function() {
+			if (!($(this).hasClass('added'))) {
+				$(this).hide();
+				console.log('hidden');
+			} else {
+				$(this).show();
+			}
+		});
+	});
+
+	$('#test-class').click(function() {
+		$('#test-description').slideToggle();
 	});
 });
